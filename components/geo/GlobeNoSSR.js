56098fa403165a2e821db89cb6faf240d008dd77
Jesper Weissglas <jesper@journeyman.se>
2021-03-31 22:05:27 +0200

    Production numbers

--- components/geo/GlobeNoSSR.js
+++ components/geo/GlobeNoSSR.js
@@ -5,14 +5,20 @@ import Spinner from "./Spinner"
 
 export default function GlobeNoSSR() {
 
-	const { data, loading } = useQuery( gql`{ neCountries { nodes { isoA2 gdpMdEst geometry } } }`, {} )
+	const {
+		data,
+		loading
+	} = useQuery( gql`{ neCountries { nodes { isoA2 countryProductionsByIso3166 { nodes { production year id } } id geometry } } } `, {} )
 
 	let polygonsData = []
 	const countries = data?.neCountries?.nodes
 	if( countries?.length > 0 )
 		polygonsData = countries
 			.filter( c => c.ISO_A2 !== 'AQ' )
-			.map( c => ( { ...c, height: Math.sqrt( c.gdpMdEst )/5000  } ) )
+			.map( c => ( {
+				...c,
+				height: Math.sqrt( c.countryProductionsByIso3166?.nodes?.find( p => p.year === 2019 )?.production ) / 250
+			} ) )
 
 	if( polygonsData.length < 10 )
 		return <Spinner/>
