9da81aa93a32fec83818a558072502c852e72da2
Jesper Weissglas <jesper@journeyman.se>
2021-04-01 08:16:01 +0200

    Year slider

--- components/geo/GlobeNoSSR.js
+++ components/geo/GlobeNoSSR.js
@@ -1,37 +1,52 @@
 import Globe from "react-globe.gl"
 import { useQuery, gql } from "@apollo/client"
 import Spinner from "./Spinner"
+import { useCallback, useEffect, useRef, useState } from "react"
 
 
-export default function GlobeNoSSR() {
+export default function GlobeNoSSR( { year } ) {
 
-	const {
-		data,
-		loading
-	} = useQuery( gql`{ neCountries { nodes { isoA2 countryProductionsByIso3166 { nodes { production year id } } id geometry } } } `, {} )
+	const { data }
+		= useQuery( gql`{ neCountries { nodes { isoA2 countryProductionsByIso3166 { nodes { production year id } } id geometry } } } `, {} )
 
-	let polygonsData = []
+	const [ polygons, set_polygons ] = useState( [] )
+	const [ duration, set_duration ] = useState( 3000 )
+	const lastYear = useRef( year )
 	const countries = data?.neCountries?.nodes
-	if( countries?.length > 0 )
-		polygonsData = countries
-			.filter( c => c.ISO_A2 !== 'AQ' )
-			.map( c => ( {
-				...c,
-				height: Math.sqrt( c.countryProductionsByIso3166?.nodes?.find( p => p.year === 2019 )?.production ) / 250
-			} ) )
 
-	if( polygonsData.length < 10 )
+	useEffect( () => {
+		if( year !== lastYear.current ) {
+			set_duration( 300 )
+			lastYear.current = year
+		}
+	}, [ year ] )
+
+	useEffect( () => {
+		if( countries?.length > 0 )
+			set_polygons(
+				countries
+					.filter( c => c.ISO_A2 !== 'AQ' )
+					.map( c => ( { ...c } ) )
+			)
+	}, [ countries ] )
+
+	const getAltitude = useCallback( country => {
+		const production = country.countryProductionsByIso3166?.nodes?.find( p => p.year === year )?.production ?? 1
+		return Math.sqrt( production ) / 250
+	}, [ year ] )
+
+	if( polygons.length < 10 )
 		return <Spinner/>
 
 	return (
 		<Globe
 			backgroundColor="#ffffff"
 			globeImageUrl="//unpkg.com/three-globe/example/img/earth-day.jpg"
-			polygonsData={polygonsData}
-			polygonAltitude={'height'}
+			polygonsData={polygons}
+			polygonAltitude={getAltitude}
 			polygonCapColor={() => 'rgba(20, 0, 0, 0.5)'}
 			polygonSideColor={() => 'rgba(0, 0, 0, 0.08)'}
-			polygonsTransitionDuration={4000}
+			polygonsTransitionDuration={duration}
 		/>
 	)
 }
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,41 @@
 import TopNavigation from "components/navigation/TopNavigation"
 import getConfig from 'next/config'
 import dynamic from "next/dynamic"
+import { Slider } from "antd"
+import { useCallback, useState } from "react"
 
 const theme = getConfig()?.publicRuntimeConfig?.themeVariables
 
 const GlobeNoSSR = dynamic( () => import( "components/geo/GlobeNoSSR" ),
 	{ ssr: false } )
 
-export default function Home( props ) {
+export default function Home() {
+	const [ year, set_year ] = useState( 2019 )
+	const handleChange = useCallback( event => {
+		set_year( event )
+	}, [] )
+
 	return (
 		<div className="page">
 
 			<TopNavigation/>
 
-			<GlobeNoSSR/>
+			<div className="content-block globe-controls">
+				<Slider
+					trackStyle={{ height: '12px' }}
+					railStyle={{ height: '12px' }}
+					handleStyle={{ height: '22px', width: '22px' }}
+					tooltipVisible={true}
+					min={2010}
+					max={2021}
+					onChange={handleChange}
+					value={year}
+				/>
+			</div>
+
+			<div className="content-block">
+				<GlobeNoSSR year={year}/>
+			</div>
 
 			<style jsx>{`
               @media (max-width: ${theme[ '@screen-sm' ]}) {
