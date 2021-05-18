2b372a4ea0a4b9d8fe32d0f5152b33b0fefb21b1
Jesper Weissglas <jesper@journeyman.se>
2021-05-18 07:46:17 +0200

    Umami logging

--- pages/_document.js
+++ pages/_document.js
@@ -7,10 +7,14 @@ export default class MyDocument extends Document {
 				<Head>
 					<link rel="icon" type="image/png" href="/favicon.png"/>
 					<link href="/fonts/style.css" rel="stylesheet"/>
+					<script
+						async defer data-website-id="1c29de0e-0dd9-435f-a24d-13a32b9d3ef0"
+						src="https://gffr-log.journeyman.se/umami.js"
+					/>
 				</Head>
 				<body>
-				<Main/>
-				<NextScript/>
+					<Main/>
+					<NextScript/>
 				</body>
 			</Html>
 		)
