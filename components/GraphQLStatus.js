ab85450ce56e085f4de51b10e88e716a75c351bd
Jesper Weissglas <jesper@journeyman.se>
2021-07-02 07:30:00 +0200

    Better error msg for networkerror

--- components/GraphQLStatus.js
+++ components/GraphQLStatus.js
@@ -2,26 +2,27 @@ import React from 'react'
 import { Alert } from "antd"
 import Loader from "./Loading"
 
-export default function GraphQLStatus(  { error, loading } ) {
-
-	if( loading ) return ( <div style={{ padding: '2rem' }}><Loader/></div> )
+export default function GraphQLStatus( { error, loading } ) {
+	if( loading ) return ( <div style={ { padding: '2rem' } }><Loader/></div> )
 
 	if( error && error instanceof Error ) {
-
 		// Format to display nicely in Alert.
-		const description = error.message
+		let description = error.message
 			.replace( /GraphQL error: /g, '' )
-			.split( '\n' ).map( ( l, i ) => <p key={i}>{l}</p> )
+			.split( '\n' ).map( ( l, i ) => <p key={ i }>{ l }</p> )
+
+		if( error.networkError ) {
+			description = 'networkError status code: ' + error.networkError.statusCode
+		}
 
 		return (
-			<div style={{ padding: '2rem' }}>
+			<div style={ { padding: '2rem' } }>
 				<Alert
 					type="error"
 					showIcon message="GraphQL Error"
-					description={description}
+					description={ description }
 				/>
 			</div> )
 	}
-
 	return null
 }
