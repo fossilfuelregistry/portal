8ca27a0d0a00969ef6e9954e9985b79cc7a2732b
Jesper Weissglas <jesper@journeyman.se>
2021-07-14 09:29:40 +0200

    Fix legacy data views.

--- queries/country.js
+++ queries/country.js
@@ -45,3 +45,24 @@ export const GQL_dataQuery = ( { iso3166, iso31662, projectId } ) => {
 		}`
 	}
 }
+
+export const GQL_countryReservesByIso = gql`
+query countryReserves($iso3166: String!) {
+  countryReserves(
+  	condition: { iso3166: $iso3166 }
+  	orderBy: YEAR_ASC
+  ) {
+    nodes { id iso3166 fossilFuelType grade sourceId unit volume quality year }
+  }
+}`
+
+export const GQL_countryProductionByIso = gql`
+query countryProductions($iso3166: String!) {
+  countryProductions(
+  	condition: { iso3166: $iso3166 }
+    filter: {fossilFuelType: {in: ["oil", "gas"]}}
+  	orderBy: YEAR_ASC
+  ) {
+    nodes { id iso3166 fossilFuelType sourceId unit volume year projection }
+  }
+}`
